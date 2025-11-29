0a7753d99d90d463e0b04f205dbd58ee8858a7ad
alejandro guttero <agutterombp@alejandros-MacBook-Pro.local>
2025-11-29 08:07:30 -0300

    change proj name to mychallenge

---
+++ mychallenge/mychallenge/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for mychallenge project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mychallenge.settings')
+
+application = get_asgi_application()
