0b883644cdc8f97ec960915b256de37707194d63
alejandro guttero <agutterombp@alejandros-MacBook-Pro.local>
2025-11-30 07:03:58 -0300

    dynamic url redirect

--- mychallenge/challenges/urls.py
+++ mychallenge/challenges/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 ## path converter
+## add name parameter to be able to user reverse and dynamic path building in views.py
 urlpatterns = [
     path("", views.home),
-    path("<int:month>", views.monthly_challenge_int),
-    path("<str:month>", views.monthly_challenge),
+    path("<int:month>", views.monthly_challenge_number),
+    path("<str:month>", views.monthly_challenge, name="month_url"),
     # path("january", views.january),
     # path("february", views.february),
 ]
--- mychallenge/challenges/views.py
+++ mychallenge/challenges/views.py
@@ -1,35 +1,81 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
 
-def january(request):
-    return HttpResponse("fn january response ok")
-
-def february(request):
-    return HttpResponse("fn february response ok")
-
 def home(request):
     return HttpResponse("fn home ok")
 
-def monthly_challenge_int(request, month):
-    print("request:", request)
-    print("month:", month)
-    print("month type:", type(month))
-    return HttpResponse(month)
+# def january(request):
+#     return HttpResponse("fn january response ok")
+
+# def february(request):
+#     return HttpResponse("fn february response ok")
+
+
+monthly_content_dict = {
+    "jan": "Eat no meat",
+    "feb": "walk 60 minutes",
+    "mar": "study 28 minutes",
+    "apr": "go cycling",
+    "may": "go trekking",
+    "jun": "go swimming",
+    "jul": "go yoga",
+    "aug": "go dancing",
+    "set": "learn django",
+    "oct": "go running",
+    "nov": "play paddle",
+    "dec": "go sailing",
+}
+
+## TEST CODE
+# dict_keys = list( monthly_content_dict.keys())
+# print ("dict_keys", dict_keys[6])
+# print ("dict_keys type", type(dict_keys))
+
+def monthly_challenge_number(request, month):
+    months = list (monthly_content_dict.keys())
+    if month > len(months) or month < 1:
+        return HttpResponseNotFound("Invalid Month")
+    redirect_month = months[month-1]
+    # print ("list:", list (monthly_content_dict.keys()))
+    # print ("month -1", month-1)
+    # print ("month -1 type ", type(month-1))
+    ## Como evitar el harcoded del "/challenges/" en V0
+    # import from django.urls import reverse
+    # add name parameter in urls.py
+    # add reverse function
+    redirect_path = reverse("month_url", args=[redirect_month]) #/challenge/jan
+    return HttpResponseRedirect(redirect_path)
+    # V0 -> return HttpResponseRedirect("/challenges/" + redirect_month )
+    #monthly_challenge(request, months[month-1]) -> esto no anda
+
+# # def monthly_challenge_int(request, month):
+#     print("request:", request)
+#     print("month:", month)
+#     print("month type:", type(month))
+#     return HttpResponse(month)
 
 
 def monthly_challenge(request, month):
-    print("request:", request)
-    print("month:", month)
-    response_text = None
-    if month == "jan":
-        response_text = "fn montly_challenge -> jan response ok"
-    elif month == "feb":
-        response_text = "fn montly_challenge -> feb response ok"
-    elif month == "mar":
-        response_text = "fn montly_challenge -> mar response ok"
-    else:
+    # print("request:", request)
+    # print("month:", month)
+    # print("month type:", type(month))
+    try:
+        challenge_text = monthly_content_dict[month]
+        return HttpResponse(challenge_text)
+    except:
         return HttpResponseNotFound("This is the text for the 404 -> month not supported")
-    return HttpResponse(response_text)
+   
+   ## V01 of fn, before of the dictionary solution 
+    # if month == "jan":
+    #     response_text = "fn montly_challenge -> jan response ok"
+    # elif month == "feb":
+    #     response_text = "fn montly_challenge -> feb response ok"
+    # elif month == "mar":
+    #     response_text = "fn montly_challenge -> mar response ok"
+    # else:
+    #     return HttpResponseNotFound("This is the text for the 404 -> month not supported")
+    # return HttpResponse(response_text)
