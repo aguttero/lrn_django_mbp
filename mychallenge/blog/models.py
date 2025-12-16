25455c038f07dd818e66b99bd2f5f665a61079d2
aguttero <aguttero@gmail.com>
2025-12-16 11:34:34 -0300

    session 126. complete DB setup and initial records

--- mychallenge/blog/models.py
+++ mychallenge/blog/models.py
@@ -5,11 +5,19 @@ from django.core.validators import MinLengthValidator
 class Tag(models.Model):
     caption = models.CharField(max_length=20)
     
+    def __str__(self):
+        return f"{self.caption}"
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email_address = models.EmailField()
+
+    def full_name(self):
+        return f"{self.last_name}, {self.first_name}"
+    
+    def __str__(self):
+        return self.full_name()
 
 class Post(models.Model):
     title = models.CharField(max_length=150)
@@ -18,4 +26,8 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True) # db_index is auto setup by SlugField
     content = models.TextField(validators=[MinLengthValidator(10)])
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts")
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts")
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
