26d7bfac85092691e641793b067c8298f65aa1f8
aguttero <aguttero@gmail.com>
2025-12-18 09:40:02 -0300

    complete manual sample html form

--- mychallenge/reviews/views.py
+++ mychallenge/reviews/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 def review(request):
-    return render(request, "reviews/review.html")
+    if request.method == 'POST':
+        entered_username = request.POST['form_item_name']
+        
+        if entered_username == "":
+            return render(request, "reviews/review.html", {
+                    "has_error": True
+                })
+        
+        print("entered_username:", entered_username)
+        return HttpResponseRedirect ("thank-you")
+    
+    return render(request, "reviews/review.html", {
+        "has_error": False
+    })
+
+def thank_you(request):
+    return render(request, "reviews/thank_you.html")
